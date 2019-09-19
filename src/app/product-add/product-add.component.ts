61c35edb0f9a1b1afd9c1526482578c49534cd3b
Lucien Turri <lucien.turri@gmail.com>
2019-09-19 19:44:14 +0200

    #Step 11: Submit Form Values To Node Server

--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,7 +1,8 @@
 // product-add.component.ts
 
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-add',
@@ -11,7 +12,7 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 export class ProductAddComponent implements OnInit {
 
   angForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private ps: ProductsService) {
     this.createForm();
   }
 
@@ -21,6 +22,10 @@ export class ProductAddComponent implements OnInit {
       ProductDescription: ['', Validators.required ],
       ProductPrice: ['', Validators.required ]
     });
+  }
+
+  addProduct(ProductName, ProductDescription, ProductPrice) {
+    this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
   }
 
   ngOnInit() {
